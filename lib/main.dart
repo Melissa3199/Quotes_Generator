352b3906fffd164ec9bcf02904ad54dd777a7869
Melissa3199 <hm_sfihi@esi.dz>
2020-03-19 20:04:52 +0100

    card-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'quote.dart';
 void main() => runApp(MaterialApp(
   home : QuotesList(),
 ));
@@ -13,14 +14,44 @@ class _QuotesListState extends State<QuotesList> {
   @override
     Random random = new Random();
     int i = 0;
-    String q = 'hi' ; 
-    List<String> quotes = [
-      'Be yourself; everyone else is already taken.',
-      'Two things are infinite: the universe and human stupidity; and Im not sure about the universe.',
-      'You only live once, but if you do it right, once is enough.',
-      'Be the change that you wish to see in the world.',
-      'Always forgive your enemies; nothing annoys them so much',
+    Quote q = new Quote() ; 
+    List<Quote> quotes = [
+      Quote(text: 'Be yourself; everyone else is already taken.', author :'Oscar Wilde'),
+      Quote(text: 'Two things are infinite: the universe and human stupidity; and Im not sure about the universe.',author :'Albert Einstein'),
+      Quote(text: 'You only live once, but if you do it right, once is enough.',author :'Mae West') , 
+      Quote(text: 'Be the change that you wish to see in the world.', author : 'Mahatma Gandhi'),
+      Quote(text: 'Always forgive your enemies; nothing annoys them so much', author : 'Oscar Wilde'),
     ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+        margin: EdgeInsets.fromLTRB(10.0, 16.0, 16.0, 0.0),
+        child : Column(
+          children: <Widget>[
+            Text(
+              q.text, 
+              style : TextStyle(
+                fontSize : 15.0,
+                letterSpacing : 2.0,
+                color : Colors.redAccent,
+              )
+            ),
+            SizedBox(
+              height : 6.0,
+            ), 
+            Text(
+              q.author, 
+              style : TextStyle(
+                fontSize : 12.0,
+                letterSpacing : 2.0,
+                color : Colors.redAccent,
+              )
+            ),
+          ],
+
+        ),
+      );
+  }
   Widget build(BuildContext context) {
     
     return Scaffold(
@@ -38,35 +69,32 @@ class _QuotesListState extends State<QuotesList> {
         ),
         centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        child: Icon(
-          Icons.add_comment,
-          color:Colors.redAccent,
-        ),
-       onPressed : (){
-          setState((){
-            i = random.nextInt(4);
-            q = quotes.elementAt(i);
-          }
-         );
-        },
-
-      ),
+   
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Center(
-          child: Text(
-            '$q',
-            style: TextStyle(
-              fontSize: 15.0,
-              letterSpacing: 2.0,
-              color : Colors.white,
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children : <Widget>[
+           quoteTemplate(q),
+           FloatingActionButton(
+            backgroundColor: Colors.white,
+            child: Icon(
+            Icons.add_comment,
+            color:Colors.redAccent,
             ),
+            onPressed : (){
+                  setState((){
+                  i = random.nextInt(4);
+                  q = quotes.elementAt(i);
+                }
+               );
+             },
 
-          ),
+           ),
+          
 
+          ],
         ),
       ),
       
